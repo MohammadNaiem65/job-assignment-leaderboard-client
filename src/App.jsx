f29445194d4e1b843f6085affb3042710e5af6e3
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-10-26 14:16:11 +0600

    update: completed Navbar design and configured App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './shared/Navbar';
 
 function App() {
     return (
-        <>
+        <main className='min-h-dvh bg-slate-200 text-slate-900'>
             <Navbar />
 
-            <Outlet />
-        </>
+            <Suspense
+                fallback={
+                    <p className='mt-[18rem] text-center text-xl font-semibold'>
+                        Loading...
+                    </p>
+                }
+            >
+                <Outlet />
+            </Suspense>
+        </main>
     );
 }
 
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,3 +1,16 @@
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
-    return <section className=''>Navbar</section>;
+    return (
+        <section className='h-20 px-10 bg-slate-900 text-white flex justify-between items-center'>
+            <Link to='/' className='text-2xl font-semibold italic'>Neina</Link>
+
+            <div className='text-lg flex gap-4'>
+                <Link to='/'>Home</Link>
+                <Link to='/dashboard'>Dashboard</Link>
+                <Link to='/login'>Login</Link>
+                <Link to='/register'>Register</Link>
+            </div>
+        </section>
+    );
 }
